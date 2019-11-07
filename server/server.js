68db923f3d3cb6cd8102286b8ae9ef614804b78b
Karen <43170840+karenminski@users.noreply.github.com>
2019-11-07 09:35:10 +0200

    Task 5

--- server/server.js
+++ server/server.js
@@ -6,11 +6,18 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const itemRouter = require("./item.router.js");
+const userRouter = require("./user.router.js");
 const DB = require("./database.js");
 const Item = require("./item.model.js");
+const bodyParser = require("body-parser");
 
 const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-3oxqq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+app.use(bodyParser.json());
+
 app.use(itemRouter);
+app.use(userRouter);
+
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
