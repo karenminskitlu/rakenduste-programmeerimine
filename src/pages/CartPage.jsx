14c4c2baf2a49164d672d02df81431acbcac2de0
Karen <43170840+karenminski@users.noreply.github.com>
2019-12-01 19:15:46 +0200

    Task 7

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -6,6 +6,7 @@ import FancyButton from "../components/FancyButton.jsx";
 import {connect} from "react-redux";
 import {removeItem} from "../store/actions.js";
 import {toast} from "react-toastify";
+import * as selectors from "../store/selectors.js";
 
 class CartPage extends React.PureComponent {
     static propTypes = {
@@ -117,7 +118,7 @@ Row.propTypes = {
 
 const mapStateToProps = (store) => {
     return {
-        cart: store.cart
+        cart: selectors.getCart(store)
     };
 };
 export default connect(mapStateToProps)(CartPage); 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import SortDropdown from "../components/SortDropdown.jsx";
 import {connect} from "react-redux";
 import {ItemProps} from "./CartPage.jsx";
 import {getItems} from "../store/actions.js";
+import * as selectors from "../store/selectors.js";
 
 class HomePage extends React.PureComponent{
 
@@ -120,7 +121,7 @@ CategoriesFilter.propTypes = {
 };
 const mapStateToProps = (store) => {
     return {
-        items: store.items,
+        items: selectors.getItems(store)
     };
 };
 export default connect(mapStateToProps)(HomePage);
