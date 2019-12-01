b241949c3db0bb6635669a81679b3194fb9bca1f
Karen <43170840+karenminski@users.noreply.github.com>
2019-12-01 19:12:44 +0200

    Task 6

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,6 +5,7 @@ import {connect} from "react-redux";
 import { userUpdate } from "../store/actions";
 import {toast} from "react-toastify";
 import "./form.css";
+import * as services from "../services.js";
 
 class LoginPage extends React.PureComponent {
     static propTypes = {
@@ -21,13 +22,7 @@ class LoginPage extends React.PureComponent {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        fetch("/api/v1/auth/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(this.state)
-        }).then( res => res.json())
+        services.login(this.state)
         .then(this.handleSuccess)
         .catch(err => {
             console.log("Error", err);
